33960b1aa408aef599bcdb53a3960623b2082905
Diego Bruno <lowendguru@gmail.com>
2024-05-07 01:23:04 +0200

    probando JNA native input

--- src/main/java/model/Stratagem.java
+++ src/main/java/model/Stratagem.java
@@ -5,67 +5,67 @@ import java.io.Serializable;
 @SuppressWarnings("serial")
 public class Stratagem implements Serializable {
 
-	private String name;
-	private String sequence;
-	private int type;
-	private String iconFileName;
-	private boolean isDlc;
+    private String name;
+    private String sequence;
+    private int type;
+    private String iconFileName;
+    private boolean isDlc;
 
-	public Stratagem() {
+    public Stratagem() {
 
-	}
+    }
 
-	public Stratagem(String name, String sequence, int type, String iconFileName) {
-		super();
-		this.name = name;
-		this.sequence = sequence;
-		this.type = type;
-		this.iconFileName = iconFileName;
-	}
+    public Stratagem(String name, String sequence, int type, String iconFileName) {
+        super();
+        this.name = name;
+        this.sequence = sequence;
+        this.type = type;
+        this.iconFileName = iconFileName;
+    }
 
-	public Stratagem(String name, String sequence, int type, String iconFileName, boolean isDlc) {
-		this(name, sequence, type, iconFileName);
-		this.isDlc = isDlc;
-	}
+    public Stratagem(String name, String sequence, int type, String iconFileName, boolean isDlc) {
+        this(name, sequence, type, iconFileName);
+        this.isDlc = isDlc;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getSequence() {
-		return sequence;
-	}
+    public String getSequence() {
+        return sequence;
+    }
 
-	public void setSequence(String sequence) {
-		this.sequence = sequence;
-	}
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
 
-	public int getType() {
-		return type;
-	}
+    public int getType() {
+        return type;
+    }
 
-	public void setType(int type) {
-		this.type = type;
-	}
+    public void setType(int type) {
+        this.type = type;
+    }
 
-	public String getIconFileName() {
-		return iconFileName;
-	}
+    public String getIconFileName() {
+        return iconFileName;
+    }
 
-	public void setIconFileName(String iconFileName) {
-		this.iconFileName = iconFileName;
-	}
+    public void setIconFileName(String iconFileName) {
+        this.iconFileName = iconFileName;
+    }
 
-	public boolean isDlc() {
-		return isDlc;
-	}
+    public boolean isDlc() {
+        return isDlc;
+    }
 
-	public void setDlc(boolean isDlc) {
-		this.isDlc = isDlc;
-	}
+    public void setDlc(boolean isDlc) {
+        this.isDlc = isDlc;
+    }
 
 }
---
+++ src/main/java/temp/User32Test.java
@@ -0,0 +1,49 @@
+package temp;
+
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.BaseTSD;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinUser;
+
+public class User32Test {
+    public static void main(String[] args) {
+        // Loop all windows
+        User32.INSTANCE.EnumWindows((hWnd, data) -> {
+            char[] name = new char[512];
+
+            User32.INSTANCE.GetWindowText(hWnd, name, name.length);
+
+            // Find window with title starting with downcase "keyb" string
+            if (Native.toString(name).toLowerCase().startsWith("new")) {
+                // Bring the window to the front
+                User32.INSTANCE.SetForegroundWindow(hWnd);
+
+                // Prepare input reference
+                WinUser.INPUT input = new WinUser.INPUT();
+
+                input.type = new WinDef.DWORD(WinUser.INPUT.INPUT_KEYBOARD);
+                input.input.setType("ki"); // Because setting INPUT_INPUT_KEYBOARD is not enough: https://groups.google.com/d/msg/jna-users/NDBGwC1VZbU/cjYCQ1CjBwAJ
+                input.input.ki.wScan = new WinDef.WORD(0);
+                input.input.ki.time = new WinDef.DWORD(0);
+                input.input.ki.dwExtraInfo = new BaseTSD.ULONG_PTR(0);
+
+                // Press "a"
+                input.input.ki.wVk = new WinDef.WORD('A'); // 0x41
+                input.input.ki.dwFlags = new WinDef.DWORD(0);  // keydown
+
+                User32.INSTANCE.SendInput(new WinDef.DWORD(1), (WinUser.INPUT[]) input.toArray(1), input.size());
+
+                // Release "a"
+                input.input.ki.wVk = new WinDef.WORD('A'); // 0x41
+                input.input.ki.dwFlags = new WinDef.DWORD(2);  // keyup
+
+                User32.INSTANCE.SendInput(new WinDef.DWORD(1), (WinUser.INPUT[]) input.toArray(1), input.size());
+
+                return false; // Found
+            }
+
+            return true; // Keep searching
+        }, null);
+    }
+}
