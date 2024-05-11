4fcba4b90ef9c9e4f31c045d29ae93dd0ae7b2f8
Diego Bruno <lowendguru@gmail.com>
2024-05-12 01:07:20 +0200

    Update stratagemLauncher.ino

--- arduino/stratagemLauncher/stratagemLauncher.ino
+++ arduino/stratagemLauncher/stratagemLauncher.ino
@@ -17,15 +17,13 @@ byte colPins[COLS] = { 5, 4, 3, 2 };  //connect to the column pinouts of the key
 //Create an object of keypad
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-int led = 13;        // the PWM pin the LED is attached to
-int brightness = 0;  // how bright the LED is
-int fadeAmount = 5;  // how many points to fade the LED by
+int led = 13;  // the PWM pin the LED is attached to
 
-int MIN_DELAY_BETWEEN_KEYSTROKE = 75;
-int MAX_DELAY_BETWEEN_KEYSTROKE = 125;
+int MIN_DELAY_BETWEEN_KEYSTROKE = 85;
+int MAX_DELAY_BETWEEN_KEYSTROKE = 135;
 
-int MIN_DELAY_PRESS_KEY = 40;
-int MAX_DELAY_PRESS_KEY = 60;
+int MIN_DELAY_PRESS_KEY = 45;
+int MAX_DELAY_PRESS_KEY = 65;
 
 void setup() {
   // declare pin 9 to be an output for LED:
@@ -38,7 +36,6 @@ void setup() {
 
 void loop() {
   char key = keypad.getKey();  // Read the key
-  // Print if key pressed
   switch (key) {
     case '1':  // reinforce
       typeSequence("wsdaw");
@@ -80,7 +77,4 @@ void delayPressKey() {
 
 void delayBetweenKeystrokes() {
   delay(random(MIN_DELAY_BETWEEN_KEYSTROKE, MAX_DELAY_BETWEEN_KEYSTROKE));
-}
-
-void ledIndication() {
 }
